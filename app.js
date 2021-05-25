1672188ce7bec903d4ba91727998f2299b898c36
Tlamatina <Tassi.lamatina@gmail.com>
2021-05-25 08:26:57 -0700

    trying to connect the dots

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
+console.log("this is working")
+
 document.addEventListener("DOMContentLoaded", () => {
-    // store constant url for dom element; request url
-    const requestUrl = "http://www.reddit.com/search.json?q=cats+nsfw:no"
+    // store constant url for form dom element; request url
+    const requestUrl = " http://www.reddit.com/search.json?q=cats+nsfw:no"
     let inputForm = document.querySelector("form")
-    let catPics = document.querySelector("#catPictures")
+    let catPictures = document.querySelector("#catPictures")
     let catRes = []
-   
-    //request data
-    //prevent form submission
+
+    // REQUEST DATA
+    // take form element and prevent default behavior
     inputForm.addEventListener("submit", (e) => {
-        e.preventDefault()
-    
-        //get user input data
+        e.preventDefault() 
+        // get user inputed number 
         let userInput = input.value
-        
-       // make fetch request to const API url
-        fetch(requestUrl + userInput)
-        console.log("input is showing")
-//             // .then --> take response data and format
-//             .then((res) => {
-//                 return res.json()
-//             })
-//             //.then --> use response JSON data
-//             .then((jsonData) => {
-//                 catsRes = jsonData.results
-//                 domCatPics(catRes)
-//                 console.log(catPics)
-//             })
-//             // .catch --> to catch errors
-//             .catch((err) => {
-//                 console.log(err)
-//                 return err
-//             })
-//     })
-// })
+        // make fetch request to const api url with given user number
+        fetch(requestUrl + userInput + "&limit=100")
+            // .then --> take response data and format
+            .then((res) => {
+                return res.json()
+            })
+            // .then --> use response JSON data
+            .then((jsonData) => {
+               catRes = jsonDat.thumbnail
+               domCatPictures(catRes)
+            })
+            // .catch --> to catch errors
+            .catch((err) => {
+                console.log(err)
+                return err
+            })
+    })
+      
+    // RESPONSE DATA
+    // collect formated data
+function domCatPictures(resArr) {
+        resArr.forEach((catPictures) => {
+            // create li element for each response
+            let div = document.createElement("div")
+            div.imgContent = catPictures
+           
+            // add li element to DOM
+            catPictures.appendChild(div)
+
+        })
+    }
+})
